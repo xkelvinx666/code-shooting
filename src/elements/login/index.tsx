a44ed6f1cc4eff8a49af821066579ca731de4792
captaincai <captain.cai@icloud.com>
2022-08-18 00:36:04 +0800

    feat: add selected map

--- src/elements/login/index.tsx
+++ src/elements/login/index.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
+import Map from '../../constants/environment/map';
+import { useState } from "react";
 import SelectMaps from "./select-maps";
 import UserName from "./user-name";
 
 export default function Login() {
+  const [userName, setUserName] = useState<string>('');
+  const [selectedMap, setSelectedMap] = useState<Map>(Map.DIRT);
   return (
     <>
-      <SelectMaps />
+      <SelectMaps map={selectedMap} setMap={setSelectedMap} />
       <UserName />
       <Link href="/game">
         <a />
--- src/elements/login/select-maps.tsx
+++ src/elements/login/select-maps.tsx
@@ -1,11 +1,25 @@
-import { useState } from 'react';
 import Image from 'next/image';
+import { useMemo } from 'react';
+import Map from '../../constants/environment/map';
+import getMapConfig from '../../assets-configs/map';
 
-export default function SelectMaps() {
-  const [map, setMap] = useState<string>('');
+interface ISelectMapsProps {
+  map: Map;
+  setMap: (map: Map) => void;
+}
+
+export default function SelectMaps({map, setMap}: ISelectMapsProps) {
+  const mapSelections = Object.keys(Map).map((name) => <div>{name}</div>);
+  const mapImage = useMemo(() => {
+    return getMapConfig()[map];
+  }, [map]);
+  console.log(mapSelections);
   return (
     <>
-      <Image src={map} />
+      <Image src={mapImage} />
+      <div>
+        {mapSelections}
+      </div>
     </>
   );
 }
