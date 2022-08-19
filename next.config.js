32ff28a11eeb93e99e270a881daa13a0831b94b3
captain.cai <captain.cai@icloud.com>
2022-08-19 17:25:41 +0800

    ci: ci

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? process.env.STATIC_URL : '',
   i18n: {
     locales: ['en-US', 'zh-CN'],
     defaultLocale: 'en-US',
