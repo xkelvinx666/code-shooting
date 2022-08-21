71be9b7ec7ec102c0f4ae9fced3f2627e414e841
captaincai <captain.cai@icloud.com>
2022-08-21 16:34:06 +0800

    ci: ci

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-  // swcMinify: true,
-  // assetPrefix: isProd ? process.env.STATIC_URL : '',
-  // i18n: {
-  //   locales: ['en-US', 'zh-CN'],
-  //   defaultLocale: 'en-US',
-  // }
+  assetPrefix: isProd ? process.env.STATIC_URL : '',
+  i18n: {
+    locales: ['en-US', 'zh-CN'],
+    defaultLocale: 'en-US',
+  }
 };
