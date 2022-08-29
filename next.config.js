b3fe83057bd24bec7f3477e2694586f770db5ebb
captain.cai <captain.cai@icloud.com>
2022-08-29 14:39:03 +0800

    refactor: next pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
   dest: 'public',
   register: true,
-  skipWaiting: true
+  skipWaiting: true,
+  runtimeCaching
 })({
   reactStrictMode: true,
   assetPrefix: isProd ? process.env.STATIC_URL : '/',
